3bc3febda97fd6c5cb0c40ea6c718c8f6deb7567
Atharva Deosthale <atharva.deosthale17@gmail.com>
2024-03-13 00:33:14 +0530

    protect routes using middleware

--- apps/server/routes/user.ts
+++ apps/server/routes/user.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { addAddress, deleteAddress, getCart, getUser } from "../functions/user";
 import { getAddresses } from "../functions/user";
+import { authenticatedUser } from "../middlewares/auth";
 
 const router = Router();
 
-router.get("/", getUser);
-router.get("/address", getAddresses);
-router.post("/address", addAddress);
-router.delete("/address/:id", deleteAddress);
-router.get("/cart", getCart);
-router.post("/cart", getCart);
-router.delete("/cart/:id", getCart);
+router.get("/", authenticatedUser, getUser);
+router.get("/address", authenticatedUser, getAddresses);
+router.post("/address", authenticatedUser, addAddress);
+router.delete("/address/:id", authenticatedUser, deleteAddress);
+router.get("/cart", authenticatedUser, getCart);
+router.post("/cart", authenticatedUser, getCart);
+router.delete("/cart/:id", authenticatedUser, getCart);
 
 export default router;
